47577af3a3c933ced0b76729bee3a0dd052589b8
SpiritDyn123 <965070149@qq.com>
2016-01-26 09:48:54 +0800

    测试性提交2

--- MyGameServer/Apps/TestCppApp/TestCppApp.cpp
+++ MyGameServer/Apps/TestCppApp/TestCppApp.cpp
@@ -5,15 +5,12 @@
 
 #include "stdafx.h"
 #include "ITest.h"
-#include "CTestDllImpted.h"
 #include "../../Dlls/TestDll/CTestDllImpted.h"
 
 int _tmain(int argc, _TCHAR* argv[])
 {
 	CTestHelper testHp;
 	testHp.Create();
-	CTestImpted obj;
-	obj.func();
 
 	return 0;
 }
